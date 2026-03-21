8086df028ad17a439b24603f70ba99a5ea29738e
ngocpqn <86608197+ngocpqn@users.noreply.github.com>
2026-03-21 11:50:40 +0700

    ok

--- app/banks.js
+++ app/banks.js
@@ -1,13 +1,12 @@
 export const BANKS = [
-{ name: "MB Bank", bin: "970422", code: "mb" }
-{ name:"MB Bank", bin:"970422", logo:"icons/mb.png" },
-{ name:"Vietcombank", bin:"970436", logo:"icons/vietcombank.png" },
-{ name:"Techcombank", bin:"970407", logo:"icons/techcombank.png" },
-{ name:"BIDV", bin:"970418", logo:"icons/bidv.png" },
-{ name:"VietinBank", bin:"970415", logo:"icons/vietinbank.png" },
-{ name:"ACB", bin:"970416", logo:"icons/acb.png" },
-{ name:"TPBank", bin:"970423", logo:"icons/tpbank.png" },
-{ name:"Sacombank", bin:"970403", logo:"icons/sacombank.png" },
-{ name:"VPBank", bin:"970432", logo:"icons/vpbank.png" },
-{ name:"Agribank", bin:"970405", logo:"icons/agribank.png" }
+  { name: "MB Bank", bin: "970422", code: "mb" },
+  { name: "Vietcombank", bin: "970436", code: "vcb" },
+  { name: "Techcombank", bin: "970407", code: "tcb" },
+  { name: "BIDV", bin: "970418", code: "bidv" },
+  { name: "VietinBank", bin: "970415", code: "icb" },
+  { name: "ACB", bin: "970416", code: "acb" },
+  { name: "TPBank", bin: "970423", code: "tpb" },
+  { name: "Sacombank", bin: "970403", code: "stb" },
+  { name: "VPBank", bin: "970432", code: "vpb" },
+  { name: "Agribank", bin: "970405", code: "agribank" }
 ]
