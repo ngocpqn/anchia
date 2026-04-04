28c1a9fe03250856a52e936119a47f6da874f790
ngocpqn <86608197+ngocpqn@users.noreply.github.com>
2026-04-05 04:09:57 +0700

    update

--- app/main.js
+++ app/main.js
@@ -1,15 +1,18 @@
 const params = new URLSearchParams(window.location.search)
 
-if(params.get("dashboard")){
+console.log("params:", window.location.search)
+
+if(params.get("dashboard") === "1"){
+  console.log("👉 vào dashboard")
+
   document.body.className = "hold-transition sidebar-mini layout-fixed"
+
   renderDashboard()
 }
 else if(params.get("view")){
-  document.body.className = ""
   renderView(params.get("view"))
 }
 else{
-  document.body.className = ""
   renderHome()
   loadBanks()
 }
